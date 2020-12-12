d53ec80bb2c178e32954b15fae5c37e93d35a43b
Sarah <sarah@sekai.co.uk>
2020-12-12 12:36:51 +0000

    feat: install sapper & ts

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,28 @@
+<GoogleAnalytics {stores} id={ga_measurment_id}/>
+
+<script lang="ts">
+	import GoogleAnalytics from "sapper-google-analytics/GoogleAnalytics.svelte";
+    import { stores } from "@sapper/app";
+	import Nav from '../components/Nav.svelte';
+
+	let ga_measurment_id = "UA-7281616-1";
+
+	export let segment: string;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>

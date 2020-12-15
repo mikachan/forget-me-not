669c8fb9ed49c90d355de9e987463ed06c6832ba
Sarah <sarah@sekai.co.uk>
2020-12-15 20:38:15 +0000

    chore(bg): upload static bg image

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,7 +5,7 @@
     import { stores } from "@sapper/app";
 	import Nav from '../components/Nav.svelte';
 	import NavMobile from '../components/NavMobile.svelte';
-	import daikon from 'images/daikon.png';
+	import bg from 'images/bg01.jpg';
 	import you from 'images/you05.jpg';
 
 	let ga_measurment_id = "UA-7281616-1";
@@ -15,24 +15,22 @@
 
 <NavMobile {segment} />
 
-<div class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+<div class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url({bg});">
 	<div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto py-32 lg:my-0">
 
 		<Nav {segment} />
 
 		<div class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0 lg:h-4/6 lg:overflow-y-scroll lg:overflow-y-visible">
-			<div class="p-4 md:p-8 text-center lg:text-left">
+			<div class="p-4 md:p-8">
 				<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url({you})"></div>
 
-				<h1 class="text-3xl font-bold pt-8 lg:pt-0">Forget-me-not: a YOU fansite</h1>
+				<h1 class="text-3xl font-bold pt-8 lg:pt-0 text-center lg:text-left">Forget-me-not: a YOU fansite</h1>
 				<div class="mx-auto lg:mx-0 w-4/5 my-3 border-b-2 border-green-500 opacity-25"></div>
 				
 				<slot></slot>
 
 				<div class="pt-6">
-					<button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
-						<a href="https://sekai.co.uk" class="text-white no-underline">Contact webmaster</a>
-					</button>
+					<p class="italic text-xs text-center lg:text-left">&copy 2004-2020. <a href="https://sekai.co.uk" class="no-underline">Contact webmaster</a>.</p>
 				</div>
 			</div>
 		</div>

ca0525313b24374182c6fddc88d1262512d35c81
Sarah Norris <sarah@sekai.co.uk>
2020-12-21 01:38:28 +0000

    chore: add more images

--- src/routes/music-career.svelte
+++ src/routes/music-career.svelte
@@ -1,3 +1,10 @@
+<script>
+	import cainsFeel from 'images/cains-feel/cainsfeel03.jpeg';
+	import gacktjob from 'images/gacktjob/gacktjob01.jpg';
+	import yfc from 'images/yfc/yfc01.jpg';
+	import sqf from 'images/sqf/sqf04.jpg';
+</script>
+
 <svelte:head>
 	<title>Music Career</title>
 </svelte:head>
@@ -10,19 +17,28 @@
 
 <h3>Pre-1989</h3>
 
-<p>You was a guitarist for a couple of different bands in Kyoto when he first started his music career. Originally, it seems that when he was younger he didn't have many ambitions for being successful outside of Kyoto, and it was Gackt who eventually persuaded him to join him in Tokyo to further his music career.</p>
+<p>You was a guitarist for a couple of different bands in Kyoto when he first started his music career. Very early in his career, it seems he didn't have many ambitions for being successful outside of Kyoto, and it was Gackt who eventually persuaded him to join him in Tokyo to further his music career.</p>
 
 <p>Read more about <a href="/you-gackt">You & Gackt here</a>.</p>
 
 <h3>CAINS:FEEL (1989-1995) <a href="/cains-feel" class="text-sm">read more &rarr;</a></h3>
 
+<figure>
+	<img alt="CAINS:FEEL" class="pb-4 mx-auto" src="{cainsFeel}">
+</figure>
+
 <p>CAINS:FEEL was a Japanese rock/goth band, formed by You and Gackt, and was their first band together.</p>
 
 <p>You met Gackt whilst he was working as a guitarist for another band. Gackt was working as a sound technician in the same studio that You's band used for recordings. Eventually, they became good friends and decided to start a band together.</p>
 
-<p>At the time, Gackt was a drummer and You was only playing guitar. They couldn't decide who should do vocals and You jokingly suggested that Gackt should do it. After a couple of arguments and proving each other wrong, Gackt agreed to be the vocalist and took singing lessons. It's fair to say that if it wasn't for You, Gackt perhaps wouldn't have even considered being a lead vocalist.</p>
+<p>At the time, Gackt was a drummer and You was playing guitar. They couldn't decide who should sing vocals and Gackt jokingly suggested that he should do it, but You just laughed. After a couple of arguments and proving each other wrong, You gave Gackt a list of songs to practice and Gackt agreed to take vocal lessons. After a week of lessons, he sung in front of You for the first time, and You's response was, "Why haven't you sung before?". It's fair to say that if it wasn't for You, Gackt perhaps wouldn't have even considered being a lead vocalist.</p>
 
 <h3>GacktJob (1999-2019) <a href="/gacktjob" class="text-sm">read more &rarr;</a></h3>
+
+<figure>
+	<img alt="GacktJob around 2001" class="pb-4 mx-auto" src="{gacktjob}">
+</figure>
+
 <p>During the time between CAINS:FEEL and GacktJob, You and Gackt kept in contact over the phone. After four years, Gackt called You up to ask him to join his band as part of his solo career. You was still living in Kyoto at the time, whilst Gackt had already moved to Tokyo to start his solo career. You didn't hesitate to join him, and moved to Tokyo soon after. It was around this time that he learnt how to play violin and piano, on Gackt's recommendation.</p>
 
 <p>You has been in JOB from the beginning of Gackt's solo career, first recording in LA in January 1999.</p>
@@ -33,11 +49,19 @@
 
 <h3>Yellow Fried Chickenz (2010-2012)</h3>
 
-<p>Yellow Fried Chickenz (also known as "YFC" or "YFCz") began as a project by Gackt to express "the cowardice in men's souls", or as an attempt to 'form' men's spirit (don't ask me to explain, all sounds very ~Gackt~). They released one studio album, one 'best of' album, a couple of singles, a live album from their first European tour and a couple of DVDs.</p>
+<figure>
+	<img alt="Yellow Fried Chickenz" class="pb-4 mx-auto" src="{yfc}">
+</figure>
 
-<p>A highlight during YFC's time was their two European tours, where they performed at sold out venues in London, Paris, Barcelona, Munich, Bochum, Cologne, Amsterdam, Berlin, Budapest, Warsaw, Leipzig, Stockholm and Moscow.</p>
+<p>Yellow Fried Chickenz (also known as "YFC" or "YFCz") began as a project by Gackt to express "the cowardice in men's souls", increase the amount of good men in the world or as an attempt to 'form'/celebrate men's spirit (don't ask me to explain, all sounds very ~Gackt~). They released one studio album, one 'best of' album, a couple of singles, a live album from their first European tour and a couple of DVDs. A lot of their songs featured English lyrics.</p>
 
-<h3>S.Q.F (2017-present)</h3>
+<p>A highlight during YFC's time was their two European tours, where they performed at sold out venues in London, Paris, Barcelona, Munich, Bochum, Cologne, Amsterdam, Berlin, Budapest, Warsaw, Leipzig, Stockholm and Moscow. This was Gackt's (and his band's) first time successfully touring outside Japan. A European tour documentary, <em>The Graffiti - Attack of The "Yellow Fried Chickenz" in Europe - "I Love You All"</em> was released on DVD in 2011.</p>
+
+<h3>S.Q.F (2015-present)</h3>
+
+<figure>
+	<img alt="S.Q.F" class="pb-4 mx-auto" src="{sqf}">
+</figure>
 
 <p>S.Q.F is a Japanese band formed in 2000 by ex-MASCHERA vocalist michi. and was originally michi.'s solo project, with ex-iLLUMINA guitarist, Nao. The name S.Q.F stands for "Spinning Q Factor."</p>
 

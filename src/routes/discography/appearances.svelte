187a4266c28709456a3acc63d5b49ed2eb799384
Sarah Norris <sarah@sekai.co.uk>
2020-12-29 01:51:48 +0000

    feat: add disco route, add more content about life film

---
+++ src/routes/discography/appearances.svelte
@@ -0,0 +1,49 @@
+<svelte:head>
+	<title>Appearances</title>
+</svelte:head>
+
+<h2>Appearances</h2>
+
+<h3>DVDs/Videos</h3>
+
+<p>Alongside live tour DVDs, You has also appeared in the following:</p>
+
+<ul>
+	<li>You played <em>Jun</em> in <a href="https://www.imdb.com/title/tt0365514/" target="_blank">Moon Child</a> (2003)</li>
+	<li><em>Life</em> video, from <em>Soyokaze</em> VHS (Gackt). <a href="discography/life-short-film">Read more &rarr;</a></li>
+	<li>Gackt PVs: <em>Mirror</em>, <em>ANOTHER WORLD</em>, <em>Juunigatsu no love song</em>, <em>Black Stone</em> &amp; <em>Redemption</em></li>
+	<li>Yellow Fried Chickenz PVs: <em>All My Love</em> &amp; <em>Mata koko de Aimashou</em></li>
+	<li>Gekkou (Gackt)</li>
+	<li>Gackt's Platinum Boxes</li>
+</ul>
+
+<h3>CDs</h3>
+<ul>
+	<li>Lyric booklet that comes with Gackt's <em>Moon</em> album</li>
+	<li>Lyric booklet that comes with Gackt's <em>The Seventh Night</em> album</li>
+</ul>
+
+<h3>Books</h3>
+<ul>
+	<li>Gackt's <em>Mizerable ~Unmei~</em> photobook</li>
+	<li>All of Gackt's tour document photobooks (<em>The Gift</em>, <em>Just Bring it!</em> etc)</li>
+	<li>The Crescent side of <em>The Air Moon</em></li>
+	<li>Gackt File 1999-2004</li>
+</ul>
+
+<h3>Magazines</h3>
+<ul>
+	<li><em>Shoxx</em> May 1999</li>
+	<li>UV 100 magazine</li>
+	<li>Motto2 vol. 01</li>
+	<li>Motto2 vol. 08</li>
+</ul>
+
+<h3>Other</h3>
+<ul>
+	<li>Gackt Dears fan club videos</li>
+	<li>Interviews with Gackt</li>
+	<li>Gackt's radio show</li>
+	<li>Modeling jewelery for <a href="http://h-darts.com" target="new">h-Darts</a></li>
+	<li><a href="http://www.executivemonthly.com/english/about/index.php" target="new">Executive Monthly</a> CM. He's sitting at the bar talking to a girl.</li>
+</ul>
--- src/routes/discography/index.svelte
+++ src/routes/discography/index.svelte
@@ -51,6 +51,14 @@
 
 <h2>Discography</h2>
 
+<h3>Visual Appearances <a href="discography/appearances" class="text-sm">read more&nbsp;&rarr;</a></h3>
+
+<p>You has appeared in many videos, books and magazines, especially from working with Gackt. The highlight obviously being his acting skillz in <em>Moon Child</em>:</p>
+
+<figure>
+	<img alt="You in Moon Child" class="p-2 pt-0 mb-2" src="{moonchild}" />
+</figure>
+
 <h3>Studio Albums</h3>
 <div class="table-container">
 	<div class="table-fade" bind:this={albumsFade}></div>
@@ -69,7 +77,7 @@
 					<td>1994</td>
 					<td>CAINS:FEEL<br> <em>demo tape</em></td>
 					<td>CAINS:FEEL</td>
-					<td>Guitar</td>
+					<td>Guitar, writer</td>
 				</tr>
 				<tr>
 					<td>1999</td>
@@ -94,6 +102,12 @@
 					<td>Moon</td>
 					<td>Gackt</td>
 					<td>Guitar, violin</td>
+				</tr>
+				<tr>
+					<td>2002</td>
+					<td>Air</td>
+					<td>Chachamaru</td>
+					<td>Writer (<em>Metamorphose</em>)</td>
 				</tr>
 				<tr>
 					<td>2003</td>
@@ -329,53 +343,3 @@
 		</table>
 	</div>
 </div>
-
-<h3>Visual Appearances</h3>
-
-<figure>
-	<img alt="You in Moon Child" class="p-2 pt-0 mb-2" src="{moonchild}" />
-</figure>
-
-<h4>DVDs/Videos</h4>
-
-<p>Alongside live tour DVDs, You has also appeared in the following:</p>
-
-<ul>
-	<li>You played <em>Jun</em> in <a href="https://www.imdb.com/title/tt0365514/" target="_blank">Moon Child</a> (2003)</li>
-	<li>Gackt PVs: <em>Mirror</em>, <em>ANOTHER WORLD</em>, <em>Juunigatsu no love song</em>, <em>Black Stone</em> &amp; <em>Redemption</em></li>
-	<li>Yellow Fried Chickenz PVs: <em>All My Love</em> &amp; <em>Mata koko de Aimashou</em></li>
-	<li>Gekkou (Gackt)</li>
-	<li><em>Life</em> video, from <em>Soyokaze</em> (Gackt)</li>
-	<li>Gackt's Platinum Boxes</li>
-</ul>
-
-<h4>CDs</h4>
-<ul>
-	<li>Lyric booklet that comes with Gackt's <em>Moon</em> album</li>
-	<li>Lyric booklet that comes with Gackt's <em>The Seventh Night</em> album</li>
-</ul>
-
-<h4>Books</h4>
-<ul>
-	<li>Gackt's <em>Mizerable ~Unmei~</em> photobook</li>
-	<li>All of Gackt's tour document photobooks (<em>The Gift</em>, <em>Just Bring it!</em> etc)</li>
-	<li>The Crescent side of <em>The Air Moon</em></li>
-	<li>Gackt File 1999-2004</li>
-</ul>
-
-<h4>Magazines</h4>
-<ul>
-	<li><em>Shoxx</em> May 1999</li>
-	<li>UV 100 magazine</li>
-	<li>Motto2 vol. 01</li>
-	<li>Motto2 vol. 08</li>
-</ul>
-
-<h4>Other</h4>
-<ul>
-	<li>Gackt Dears fan club videos</li>
-	<li>Interviews with Gackt</li>
-	<li>Gackt's radio show</li>
-	<li>Modeling jewelery for <a href="http://h-darts.com" target="new">h-Darts</a></li>
-	<li><a href="http://www.executivemonthly.com/english/about/index.php" target="new">Executive Monthly</a> CM. He's sitting at the bar talking to a girl.</li>
-</ul>

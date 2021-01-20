a4c4b4249cc3d8c2136a4286597187dc825ab272
Sarah Norris <sarah@sekai.co.uk>
2021-01-20 02:06:25 +0000

    chore: add tests for Nav & NavContent

--- src/components/__tests__/Nav.test.ts
+++ src/components/__tests__/Nav.test.ts
@@ -4,19 +4,16 @@ import { fireEvent, render } from '@testing-library/svelte';
 
 import Nav from '../Nav.svelte';
 
-test('shows proper heading when rendered', () => {
-	const { getByText } = render(Nav, { segment: 'Home' });
+test('renders Nav with Menu title', () => {
+	const { getByText } = render(Nav, { segment: '' });
 
 	expect(getByText('Menu')).toBeInTheDocument();
 });
 
-// Note: This is as an async test as we are using `fireEvent`
-// test('changes button text on click', async () => {
-// 	const { getByText } = render(Nav, { segment: 'World' });
-// 	const button = getByText('Button');
+// test('clicking button triggers openNav function', async () => {
+// 	const { getByRole } = render(Nav, { segment: 'Home' });
+// 	const button = getByRole('button');
 
-// 	// Using await when firing events is unique to the svelte testing library because
-// 	// we have to wait for the next `tick` so that Svelte flushes all pending state changes.
 // 	await fireEvent.click(button);
 
 // 	expect(button).toHaveTextContent('Button Clicked');

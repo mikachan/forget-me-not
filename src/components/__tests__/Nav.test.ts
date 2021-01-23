bae1fdf84a1f2693175e51248585c99b0cf32443
Sarah Norris <sarah@sekai.co.uk>
2021-01-23 15:39:21 +0000

    test: add tests for Link and NavMobile

--- src/components/__tests__/Nav.test.ts
+++ src/components/__tests__/Nav.test.ts
@@ -1,20 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import { fireEvent, render } from '@testing-library/svelte';
-
 import Nav from '../Nav.svelte';
+import { render } from '@testing-library/svelte';
 
 test('renders Nav with Menu title', () => {
 	const { getByText } = render(Nav, { segment: '' });
 
 	expect(getByText('Menu')).toBeInTheDocument();
 });
-
-// test('clicking button triggers openNav function', async () => {
-// 	const { getByRole } = render(Nav, { segment: 'Home' });
-// 	const button = getByRole('button');
-
-// 	await fireEvent.click(button);
-
-// 	expect(button).toHaveTextContent('Button Clicked');
-// });
--- src/components/__tests__/NavContent.test.ts
+++ src/components/__tests__/NavContent.test.ts
@@ -3,8 +3,19 @@ import '@testing-library/jest-dom/extend-expect';
 import NavContent from '../NavContent.svelte';
 import { render } from '@testing-library/svelte';
 
-test('renders home link in nav', () => {
-	const { getByText } = render(NavContent, { segment: '' });
+test('renders home link in nav with active class', () => {
+	const { getByText } = render(NavContent, { segment: undefined });
+	const homeLink = getByText('Home').parentElement;
 
-	expect(getByText('Home')).toBeInTheDocument();
+	expect(homeLink).toBeInTheDocument();
+	expect(homeLink).toHaveClass('active');
+});
+
+test('sets aria-current and active class when home is not the current page', () => {
+	render(NavContent, { segment: 'about-you' });
+	const currentMenuItem = document.querySelector('[aria-current="page"]');
+
+	expect(currentMenuItem).toBeInTheDocument();
+	expect(currentMenuItem.innerHTML).toContain('About YOU');
+	expect(currentMenuItem).toHaveClass('active');
 });

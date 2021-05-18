ea49404e53e23e48eb17cb96d998453caaa20406
Sarah Norris <sarah@sekai.co.uk>
2021-05-18 13:46:20 +0100

    chore: update news

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,6 +11,6 @@
 <h2>Latest Site Updates <a href="site/updates" class="text-sm">read more&nbsp;&rarr;</a></h2>
 
 <p>
-	<strong>16.02.21</strong><br>
-	Ded Chaplin and Luscious are streaming two live performances on 17th and 18th February, from The Doors in Hatsudai. You can purchase tickets to watch the performances live through <Link href="https://twitcasting.tv/chachamaru_yfcz">TwitCasting</Link>, which also gives you access to watch the stream recording up to 2 weeks from the original broadcast. See <Link href="https://twitter.com/CHACHAMARU_YFCz/status/1359381600425758722">Chacha's Twitter</Link> for more info. Looking forward to it!
+	<strong>18.05.21</strong><br>
+	Luscious have announced they're going to be doing another live performance on June 17th! It'll be at Club Phase Takadanobaba in Tokyo, and will be streamed on <Link href="https://twitcasting.tv/chachamaru_yfcz">TwitCasting</Link> again. For full details, see <Link href="https://twitter.com/CHACHAMARU_YFCz/status/1394626299394625538">Chacha's Twitter</Link>.
 </p>

3ef780aca119afe14ca08e4dbff74e4064d87c6c
Sarah Norris <sarah@sekai.co.uk>
2021-10-17 18:17:36 +0100

    feat: install svelte-image

--- forget-me-not/svelte.config.js
+++ forget-me-not/svelte.config.js
@@ -1,14 +1,23 @@
 import preprocess from 'svelte-preprocess';
+import svelteImage from 'svelte-image';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), svelteImage()],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			optimizeDeps: {
+				include: ['blurhash']
+			},
+			ssr: {
+				noExternal: ['svelte-image']
+			}
+		}
 	}
 };
 

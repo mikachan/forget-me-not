c1e3ad8ecbe0d9723cf05336075b125023486fed
Sarah Norris <sarah@sekai.co.uk>
2021-11-15 21:53:57 +0000

    fix: change order of preprocess list

--- svelte.config.js
+++ svelte.config.js
@@ -3,27 +3,26 @@ import svelteImage from 'svelte-image';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-		svelteImage()
-	],
-
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {
-				include: ['blurhash']
+				include: ['blurhash'],
 			},
 			ssr: {
-				noExternal: ['svelte-image']
-			}
-		}
-	}
+				noExternal: ['svelte-image'],
+			},
+		},
+	},
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true,
+			...svelteImage(),
+		}),
+	],
 };
 
 export default config;

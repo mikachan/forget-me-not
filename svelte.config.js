62f7c807e32cccee25f8db7049205ce8a5eacc85
Sarah Norris <sarah@sekai.co.uk>
2021-12-23 20:04:55 +0000

    feat: install netlify adapter & related settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 import svelteImage from 'svelte-image';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {
